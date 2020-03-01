0a298d8784d70e7e6c38ac3c7c13ddee79bfbb2d
david-vloedman <runner@fv-az32.ewojef221inudobrzhtqmuqrrb.cx.internal.cloudapp.net>
2020-03-01 17:35:25 +0000

    Updates

---
+++ precache-manifest.338efe692980af2c3b2f4e67ac783327.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b3bbbdfeb7aacca46d33615d56f6285",
+    "url": "/lab-32-context/index.html"
+  },
+  {
+    "revision": "d0e7edc890ea5d71084f",
+    "url": "/lab-32-context/static/css/main.263174df.chunk.css"
+  },
+  {
+    "revision": "7e43de888f099857f579",
+    "url": "/lab-32-context/static/js/2.fa022edf.chunk.js"
+  },
+  {
+    "revision": "d0e7edc890ea5d71084f",
+    "url": "/lab-32-context/static/js/main.98b261cd.chunk.js"
+  },
+  {
+    "revision": "a34dbb19385c15f893a0",
+    "url": "/lab-32-context/static/js/runtime~main.fcc7fb8a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f83daf492cf6f27c9b4ab432b7a72fad.js"
+  "/lab-32-context/precache-manifest.338efe692980af2c3b2f4e67ac783327.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lab-32-context/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
